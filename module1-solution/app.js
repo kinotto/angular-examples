c2b743f1101457fc5883fb3991a8384e52ac3d21
Abdelcadir Karim <AbdelcadirKa@valueteam.com>
2017-02-06 15:12:47 +0100

    added optional exercise (bonus)

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,25 +6,55 @@
 
   function LunchCheckController($scope){
     $scope.userInput = '';
-
+    var results = [
+      {
+        message: 'Enjoy!',
+        style: {
+          color: 'green',
+          border: '1px solid green'
+        }
+      },
+      {
+        message: 'Too much!',
+        style: {
+          color: 'green',
+          border: '1px solid green'
+        }
+      },
+      {
+        message: 'Please enter data first',
+        style: {
+          color: 'red',
+          border: '1px solid red'
+        }
+      }
+    ]
     $scope.checkTooMuch = function(){
       var inputArray = $scope.userInput.split(',');
       cleanEmptyString(inputArray);
       if(inputArray.length > 0 && inputArray.length  <= 3){
-        $scope.result = 'Enjoy!';
+        $scope.result = results[0];
       }
       if(inputArray.length > 3){
-        $scope.result = 'Too much!';
+        $scope.result = results[1];
       }
       if(inputArray.length === 0){
-        $scope.result = 'Please enter data first';
+        $scope.result = results[2];
       }
     }
 
     var cleanEmptyString= function(inputArray){
-      if(inputArray.length === 1 && inputArray[0] === ''){
-        inputArray.splice(0, 1);
+      if(inputArray.length === 1 && inputArray[0].trim() === ''){
+        return inputArray.splice(0, 1);
       }
+      for(var i = 0;  i < inputArray.length; i++){
+        if(inputArray[i].trim() === ''){
+          inputArray.splice(i, 1);  //remove empty string  eg. item1, , item2 
+          i--; // without this we skip an element
+        }
+      }
+
+
     }
 
   }
