732bfe47a4fa11249b5e74f6c68472c953f47788
Abdelcadir Karim <AbdelcadirKa@valueteam.com>
2017-02-24 11:42:16 +0100

    fix ng-blur module5

--- module5-solution/src/public/subscription/subscription.js
+++ module5-solution/src/public/subscription/subscription.js
@@ -1,22 +1,36 @@
 angular.module('public')
 .controller('subscriptionCtrl', subscriptionCtrl);
 
-subscriptionCtrl.$inject = ['subscriptionService', '$scope'];
+subscriptionCtrl.$inject = ['subscriptionService', '$scope', '$q'];
 
-function subscriptionCtrl(subscriptionService, $scope){
+function subscriptionCtrl(subscriptionService, $scope, $q){
   var _this = this;
+  var menuItem;
 
+  _this.checkMenuItem = function(menuNumber){
+    var deferred = $q.defer();
+    _this.infoSaved = false;
+    var promise =  subscriptionService.getMenuItemsByShortName(menuNumber);
+    promise.then(function(result){
+      var menuItem = result.data;
+      deferred.resolve();
+    })
+    .catch(function(err){
+      $scope.subscriptionForm.menuNumber.$invalid = true;
+      $scope.subscriptionForm.$invalid = true;
+      console.log(err);
+      deferred.reject();
+    })
+
+    return deferred.promise;
+  }
 _this.saveUserData = function(userData){
-  var promise =  subscriptionService.getMenuItemsByShortName(userData.menuNumber);
-  promise.then(function(result){
-    userData.menuItem = result.data;
-    subscriptionService.saveUserData(userData);
-    _this.shortNameIsValid = true;
-  })
-  .catch(function(err){
-    $scope.subscriptionForm.menuNumber.$invalid = true;
-     _this.shortNameIsValid = false;
-    console.log(err);
-  })
+    _this.checkMenuItem(userData.menuNumber) //check menu number for safety, could happen that ng-blur does not trigger
+    .then(function(){
+      userData.menuItem = menuItem;
+      subscriptionService.saveUserData(userData);
+      _this.infoSaved = true;
+    })
+    
   }
 }
